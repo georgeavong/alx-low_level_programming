bd27977bb4de557ce6647b7d9363d95bcdae5999
georgeavong <georgeavong@gmail.com>
2023-09-17 21:47:19 +0000

    A C program that prints size of format

---
+++ 4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * A C program that prints some text using the puts function
+ *
+ * returns the value of 0
+ */
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+
+}
---
+++ 5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/** main - outputs "with proper grammar, but the outcome is a piece of art," using the printf function
+ *
+ * return - returns the value of 0
+ */
+int main()
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+
+}
